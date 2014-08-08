c2fdea25090fb88832330821eddc4214e25aaffb
Komatsu <school@athenaeum-society.com>
2014-08-08 20:12:44 +0900

    Create HelloScript.cs
    Unityの基本動作に関するものを、Consoleビューに表示させます。

---
+++ HelloScript.cs
@@ -0,0 +1,13 @@
+public class TestScript : MonoBehaviour
+{
+ public int num = 1000;
+ void Start() {
+  Debug.Log(num);
+  Debug.Log(num+500);
+  Debug.Log("テスト");
+  Debug.Log(GetComponent<TestScript>().num);
+  Debug.Log(this.num);
+ }
+ void Update() {
+ }
+}
