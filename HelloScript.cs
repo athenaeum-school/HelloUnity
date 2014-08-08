9f0a4908a174acb6a0d7f516ffce5d76abb8f373
Komatsu <school@athenaeum-society.com>
2014-08-08 20:24:11 +0900

    Update HelloScript.cs

--- HelloScript.cs
+++ HelloScript.cs
@@ -1,11 +1,11 @@
-public class TestScript : MonoBehaviour
+public class HelloScript : MonoBehaviour
 {
  public int num = 1000;
  void Start() {
   Debug.Log(num);
   Debug.Log(num+500);
   Debug.Log("テスト");
-  Debug.Log(GetComponent<TestScript>().num);
+  Debug.Log(GetComponent<HelloScript>().num);
   Debug.Log(this.num);
  }
  void Update() {
